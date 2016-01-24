ba856de7c3e17b73731efb774ba2334e96379f34
Konstantin <hs@ngs.ru>
2016-01-24 21:32:52 +0600

    channel initialization

---
+++ src/main/java/server/impl/ServerObjectChannelInitializer.java
@@ -0,0 +1,39 @@
+package server.impl;
+
+import client.ResponseHandler;
+import client.impl.ServerResponseAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
+import io.netty.handler.ssl.SslHandler;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLEngine;
+
+public class ServerObjectChannelInitializer<ResponseDto> extends ChannelInitializer<SocketChannel> {
+
+    private SSLContext sslContext;
+    private ResponseHandler<ResponseDto> responseHandler;
+
+    public ServerObjectChannelInitializer(SSLContext sslContext, ResponseHandler<ResponseDto> responseHandler) {
+        this.sslContext = sslContext;
+        this.responseHandler = responseHandler;
+    }
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline p = ch.pipeline();
+
+        SSLEngine engine = sslContext.createSSLEngine();
+        engine.setUseClientMode(true);
+        engine.setNeedClientAuth(true);
+
+        p.addLast("ssl", new SslHandler(engine));
+        p.addLast(new ObjectEncoder());
+        p.addLast(new ObjectDecoder(ClassResolvers.cacheDisabled(null)));
+        p.addLast(new ServerResponseAdapter(responseHandler));
+    }
+}
